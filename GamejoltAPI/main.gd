1e3bedda55edaa4630b5cd80c4c698e62c724d83
MrNaru300 <bruno.santi.oli@gmail.com>
2020-07-04 17:08:12 -0300

    Refatorando o codigo

--- GamejoltAPI/main.gd
+++ GamejoltAPI/main.gd
@@ -4,6 +4,8 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_custom_type("GameJoltAPI", "HTTPRequest", preload("API.gd"), preload("gamejolt.png"))
+	add_autoload_singleton("GameJoltAPI", "res://addons/GamejoltAPI/API.gd")
 
 func _exit_tree():
 	remove_custom_type("GameJoltAPI")
+	remove_autoload_singleton("GameJoltAPI")
