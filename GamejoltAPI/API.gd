31c84bd2f4f5c93a9c31a7d3da4c6e2ed3ef633d
MrNaru300 <bruno.santi.oli@gmail.com>
2020-07-05 16:37:26 -0300

    Godot API funcionando

--- GamejoltAPI/API.gd
+++ GamejoltAPI/API.gd
@@ -1,16 +1,28 @@
 extends HTTPRequest
 
-export(String) var gameId
-export(String) var privateKey
 export(String) var username
 export(String) var user_token
 
-
 const base_link = "https://api.gamejolt.com/api/game/"
+var gameID : String
+var privateKey : String
+
+func _ready():
+	connect("request_completed", self, "_on_request_completed")
+	
+	
+func _on_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
+	print("Request completed (Response code = %d)!" % response_code)
+
 
 func request(url: String, custom_headers:=PoolStringArray(), ssl_validate_domain:=true, method:=0, request_data:=""):
+	
+	
+	privateKey = ProjectSettings.get_setting("GameJoltAPI/PrivateKey")
+	gameID = ProjectSettings.get_setting("GameJoltAPI/GameID")
+	
 	var formated_URL = url_format(url, {
-		"game_id":		gameId,
+		"game_id":		gameID,
 		"username":		username,
 		"user_token":	user_token
 	})
@@ -18,24 +30,26 @@ func request(url: String, custom_headers:=PoolStringArray(), ssl_validate_domain
 	if (privateKey):
 		formated_URL = sign_url(formated_URL)
 	
+	print("Making request to GameJolt!...")
+	print(formated_URL)
 	.request(formated_URL, custom_headers, ssl_validate_domain, method, request_data)
 
 
-func auth_user(username, user_token):
+func auth_user():
 	request(base_link + "v1" + "/users/auth/")
 	
-func add_achieved(username, user_token, trophy_id):
+func add_achieved(trophy_id):
 	request(url_format(
 		base_link + "v1_2" + "/trophies/add-achieved/", { "trophy_id": String(trophy_id) }
 	))
 	
-func remove_achieved(username, user_token, trophy_id):
+func remove_achieved(trophy_id):
 	request(url_format(
 		base_link + "v1_2" + "/trophies/remove-achieved/", { "trophy_id": String(trophy_id) }
 	))
 	
 	
-func fetch_achieved(username, user_token, trophy_id=null, achieved=null):
+func fetch_achieved(trophy_id=null, achieved=null):
 	var parameters = {}
 	if (trophy_id): parameters["trophy_id"] = trophy_id
 	if (achieved): parameters["achived"] = achieved
@@ -43,20 +57,23 @@ func fetch_achieved(username, user_token, trophy_id=null, achieved=null):
 	request(url_format(base_link + "v1" + "/trophies/", parameters))
 	
 	
-func open_session(username, user_token):
+func open_session():
 	request(base_link + "v1" + "/sessions/open/")
 	
 func url_format (base: String, args: Dictionary = {}) -> String:
 	
 	var link = base
 	
-	if (!base.match("\\?")):
-		link += "?"
-	else:
-		link += "&"
+	if (!base.ends_with("?")):
+		if (base.find("?")):
+			link += "&"
+		else:
+			link += "?"
+			
 	
 	for key in args:
-		link += String(key).strip_edges().http_escape() + "=" + String(args[key]).strip_edges().http_escape() + "&"
+		if (args[key]):
+			link += String(key).strip_edges().http_escape() + "=" + String(args[key]).strip_edges().http_escape() + "&"
 	
 	link = link.rstrip("&")
 	
@@ -66,4 +83,4 @@ func sign_url(url: String) -> String:
 	var signature = url + privateKey
 	signature = signature.sha1_text().http_escape()
 	
-	return url + "&signature=" + signature
+	return url_format(url, {"signature": signature})
--- GamejoltAPI/main.gd
+++ GamejoltAPI/main.gd
@@ -5,7 +5,34 @@ extends EditorPlugin
 func _enter_tree():
 	add_custom_type("GameJoltAPI", "HTTPRequest", preload("API.gd"), preload("gamejolt.png"))
 	add_autoload_singleton("GameJoltAPI", "res://addons/GamejoltAPI/API.gd")
+	
+
+	add_custom_project_setting("GameJoltAPI/GameID", "", TYPE_STRING)
+	add_custom_project_setting("GameJoltAPI/PrivateKey", "", TYPE_STRING)
+	
+	
+	var error := ProjectSettings.save()
+	if error: push_error("Encountered error %d when saving project settings." % error)
+	
+
+func add_custom_project_setting(name: String, default_value, type: int, hint: int = PROPERTY_HINT_NONE, hint_string: String = "") -> void:
+
+	if ProjectSettings.has_setting(name): return
+
+	var setting_info: Dictionary = {
+		"name": name,
+		"type": type,
+		"hint": hint,
+		"hint_string": hint_string
+	}
+
+	ProjectSettings.set_setting(name, default_value)
+	ProjectSettings.add_property_info(setting_info)
+	ProjectSettings.set_initial_value(name, default_value)
 
 func _exit_tree():
 	remove_custom_type("GameJoltAPI")
 	remove_autoload_singleton("GameJoltAPI")
+	ProjectSettings.clear("GameJoltAPI/PrivateKey")
+	ProjectSettings.clear("GameJoltAPI/GameID")
+	
